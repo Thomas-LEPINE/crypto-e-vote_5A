2c890f76d0786adac6b14ec1359a0ff28b80d36a
Alexis5998 <alexis.goiset98@gmail.com>
2020-11-16 19:14:43 +0100

    ModifAdmin

--- administrateur.py
+++ administrateur.py
@@ -1,13 +1,38 @@
 from helper import *
 from Decompteur import *
+
 class Administrator(object):
     def __init__(self):
         self.dec = Decompteur(53, 11, 27, 5)
         return
-    def Contact(self, cle, commissaire):
-        return commissaire.Verify(cle)
-    def BlindSign(self, mp):
+
+    '''
+    Prends le commissaire et lui demande de verifier la clé
+    Dit Oui si peut voter, sinon non
+    '''
+    def Contact(self):
+        print ("La clé est : ")
+        cle=input()
+        print ("La commissaire est : ")
+        commissaire=input()
+        #return commissaire.Verify(cle)
+        print("Le commissaire est : ", commissaire)
+        print("La clé est : ", cle)
+
+    '''
+    Signe le mp
+    '''
+    def BlindSign(self):
+        mp = InputArray("Le mp est : ")
         array = []
         for i in range(0, len(mp)):
             array.append(pow(mp[i], self.dec.d) % self.dec.n)
-        return array
+        print("Le mpp est : ", array)
+
+#Test
+
+admin=Administrator()
+com1=Commissaire()
+admin.Contact()
+admin.BlindSign()
+
